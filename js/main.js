a04e93eaa83b0f10fec9c0fca7592725840ef513
Legich13 <60274918+Legich13@users.noreply.github.com>
2020-09-24 16:38:40 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-alert('hellow world')
+const firstRow = 'мама мыла раму';
+const secondRow = 'собака друг человека';
+
+function getRow(firstRow, secondRow){
+   let str1 = (firstRow.split("а").length - 1);
+   let str2 = (secondRow.split("а").length - 1);
+ for (let index = 0; index < str1 ; index++) {
+     if(str1 > str2) {
+     return console.log(firstRow)
+     } else {
+     return console.log(secondRow)
+     }
+ }
+}
+getRow(firstRow, secondRow);
